dc655b00d7723729c9b0513a12ca2c5f98910f4c
RRBlackburn <31480461+RRBlackburn@users.noreply.github.com>
2017-10-12 19:51:33 -0400

    Website Progress
    Moved Columns Around, Fixing Javascript for Nav Bar, Fixed Colors, Added and linked pages successfully (WOO!)

--- js/main.js
+++ js/main.js
@@ -1,13 +1,14 @@
 /* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
-function myFunction() {
-    var x = document.getElementById("myTopnav");
-    if (x.className === "topnav") {
-        x.className += " responsive";
+function myfunction() {
+    var x = document.getElementById("mytopnav");
+    if (x.style.display === "none") {
+        x.style.display = "block";
     } else {
-        x.className = "topnav";
+        x.style.display = "none";
     }
 }
 
+/*
 var slideIndex = 1;
 showSlides(slideIndex);
 
@@ -33,4 +34,4 @@ function showSlides(n) {
   }
   slides[slideIndex-1].style.display = "block";
   dots[slideIndex-1].className += " active";
-}
+}*/
